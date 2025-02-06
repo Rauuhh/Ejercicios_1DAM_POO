d311f4ba3a758d5322f3ce5acdc31fb5a9737e45
RaulCasino <raildetrenes@gmail.com>
2025-02-06 11:35:58 +0100

    Update Cadena.java

--- POO/src/main/java/org/example/Formiguero/Cadena.java
+++ POO/src/main/java/org/example/Formiguero/Cadena.java
@@ -1,18 +1,9 @@
-package org.example.Formiguero;
-
-import org.example.Biblioteca.Libro;
-
+package org.example;
 import java.util.ArrayList;
-
 public class Cadena {
+    //Creo las variables del nombre y la cantidas de programas que tentra nuestra cadenna
     private String nombre;
     private ArrayList<Programa> listaProgramas;
-
-    public Cadena(String nombre){
-        this.nombre = nombre;
-        listaProgramas = new ArrayList<>();
-    }
-
     public String getNombre() {
         return nombre;
     }
@@ -29,11 +20,30 @@ public class Cadena {
         this.listaProgramas = listaProgramas;
     }
 
-    public void anyadirPrograma(Programa programa){
-        listaProgramas.add(programa);
+    public Cadena(){
+        this.listaProgramas = new ArrayList<>();
     }
+
+    public Cadena(String nombre) {
+        this.nombre = nombre;
+        listaProgramas = new ArrayList<>();
+
+    }
+
+    public void agregarPrograma(Programa programa) {
+        //metodo para agregar programas
+        listaProgramas.add(programa);
+        programa.setCadena(this);
+    }
+
+    public void borrarPrograma(Programa programa) {
+        //metodo para borrar programas
+        listaProgramas.remove(programa);
+        programa.setCadena(new Cadena());
+    }
+
     @Override
     public String toString() {
-        return "Cadena{" + "nombre='" + nombre + '\'' + ", listaProgramas=" + listaProgramas + '}';
+        return "Cadena{" + "nombre='" + nombre +  ", listaProgramas=" + listaProgramas + '}';
     }
 }
