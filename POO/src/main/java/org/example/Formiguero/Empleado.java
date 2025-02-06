418e1368d54a883e14b0b33b6a3faa9149044c1f
RaulCasino <raildetrenes@gmail.com>
2025-02-06 11:36:24 +0100

    Update Empleado.java

--- POO/src/main/java/org/example/Formiguero/Empleado.java
+++ POO/src/main/java/org/example/Formiguero/Empleado.java
@@ -1,63 +1,83 @@
-package org.example.Formiguero;
+package org.example;
 
-public class Empleado {
-    final static String EP = "EP";
-    public static int cantidadEmpleados = 0;
-    private String id;
-    private String nombre;
-    private String cargo;
-    private Empleado director;
+    public class Empleado {
+        //Creo las clases que necesita empleado
+        private String id;
+        private String nombre;
+        private String cargo;
+        private Empleado director;
+        private static int cantidad = 0;
 
-    public Empleado(String nombre, String cargo){
-        id = EP + calcularID(cantidadEmpleados);
-        cantidadEmpleados++;
-        this.nombre = nombre;
-        this.cargo = cargo;
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public String getNombre() {
+            return nombre;
+        }
+
+        public void setNombre(String nombre) {
+            this.nombre = nombre;
+        }
+
+        public String getCargo() {
+            return cargo;
+        }
+
+        public void setCargo(String cargo) {
+            this.cargo = cargo;
+        }
+
+        public Empleado getDirector() {
+            return director;
+        }
+
+        public void asignarDirector(Empleado director) {
+            this.director = director;
+        }
+
+        public String generarID() {
+            //metodo para generar automaticamente una ID a cada empleando que empiece por EP
+
+            return String.format("EP%03d", ++cantidad);
+
+        }
+
+        public String comprobarCargo(String cargo) {
+            // metodo para indicarle que si no tiene ninguno de estos cargos le ponga pte
+
+            if (!cargo.equals("director") && !cargo.equals("técnico") && !cargo.equals("presentador") && !cargo.equals("colaborador"))
+                return "pte";
+            return cargo;
+
+        }
+
+        public Empleado(String nombre, String cargo) {
+            this.nombre = nombre;
+            this.cargo = comprobarCargo(cargo);
+            id = generarID();
+            this.director = null;
+        }
+
+        public void setDirector(Empleado director) {
+            this.director = director;
+        }
+
+        public static int getCantidad() {
+            // metodo para que te devuelva el numero de empleados
+            return cantidad;
+        }
+        @Override
+        public String toString() {
+            return "Empleado{" +
+                    "id='" + id + '\'' +
+                    ", nombre='" + nombre + '\'' +
+                    ", cargo='" + cargo + '\'' +
+                    ", director=" + director;
+        }
     }
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public Empleado getDirector() {
-        return director;
-    }
-
-    public void setDirector(Empleado director) {
-        this.director = director;
-    }
-
-    public String getCargo() {
-        return cargo;
-    }
-
-    public void setCargo(String cargo) {
-        this.cargo = cargo;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public static int getCantidadEmpleados() {
-        return cantidadEmpleados;
-    }
-
-    public static void setCantidadEmpleados(int cantidadEmpleados) {
-        Empleado.cantidadEmpleados = cantidadEmpleados;
-    }
-
-    public static String calcularID(int cantidadEmpleados) {
-
-        return String.format("%03d", cantidadEmpleados);
-    }
-
-}
 
